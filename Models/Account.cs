5eb2fbf2b04da0f884c0ef05f564a99426fe9d60
Kion Shamsa <kionkshamsa@gmail.com>
2018-10-06 12:39:37 -0600

    Worked on trying to fix the database connections, currently still not working

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,9 +9,11 @@ namespace WeberPage.Models
     public class Account
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Username is required")]
         [MinLength(4, ErrorMessage ="Username must be at least 4 characters")]
         public string Username { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
         [DataType(DataType.Password)]
